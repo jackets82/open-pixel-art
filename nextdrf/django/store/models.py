691fbc53687c6c2006b0a0852a40188d8a5c82ab
jackets82 <ad_coates@hotmail.com>
2021-12-24 20:37:22 +0000

    master

---
+++ nextdrf/django/store/models.py
@@ -0,0 +1,81 @@
+from functools import cmp_to_key
+
+from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Category(MPTTModel):
+    """
+    Category Table implemented with MPTT.
+    """
+
+    name = models.CharField(
+        verbose_name=_("Category Name"),
+        help_text=_("Required and unique"),
+        max_length=255,
+        unique=True,
+    )
+    slug = models.SlugField(verbose_name=_("Category safe URL"), max_length=255, unique=True)
+    parent = TreeForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name="children")
+    is_active = models.BooleanField(default=True)
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+
+    def get_absolute_url(self):
+        return reverse("store:category_list", args=[self.slug])
+
+    def __str__(self):
+        return self.name
+
+
+class ProductType(models.Model):
+    """
+    ProductType Table will provide a list of the different types of products that are for sale.
+    """
+
+    name = models.CharField(verbose_name=("Product Name"), help_text=("Required"), max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = _("Product Type")
+        verbose_name_plural = _("Product Types")
+
+    def __str__(self):
+        return self.name
+
+
+class ProductSpecification(models.Model):
+    """
+    The product specification table contains product specification of features for the product types.
+    """
+
+    product_type = models.ForeignKey(ProductType, on_delete=models.RESTRICT)
+    name = models.CharField(verbose_name=_("Name"), help_text=_("Required"), max_length=255)
+
+    class Meta:
+        verbose_name = _("Product Specification")
+        verbose_name_plural = _("Product Specifications")
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """
+    The Product Table containing all product items
+    """
+
+    product_type = models.ForeignKey(ProductType, on_delete=models.RESTRICT)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    title = models.CharField(
+        verbose_name=("title"),
+        help_text=_("Required"),
+        max_length=255,
+    )
